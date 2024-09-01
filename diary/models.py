99a101178eec696f69eb5b4bc62199a6fa41c559
FlintJames <lyuciana@yandex.ru>
2024-09-01 20:11:00 +0300

    change models.py

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    image = models.ImageField(
+        upload_to="diary/", **NULLABLE, verbose_name="Изображение"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    publication_sign = models.BooleanField(default=True, verbose_name="Публикация")
+    number_of_views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+    views_counter = models.PositiveIntegerField(verbose_name="Счётчик просмотров", default=0)
+
+    owner = models.ForeignKey(User, default=True, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    def str(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+        ordering = ["title", "content", "publication_sign"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name="Страна", **NULLABLE)
+
+    token = models.CharField(max_length=100, verbose_name="Токен", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def str(self):
+        return f"{self.email}"
