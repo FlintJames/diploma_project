ede8f40696ff22c8f9c761758323cb99e010f280
FlintJames <lyuciana@yandex.ru>
2024-09-03 05:42:27 +0300

    change html change urls.py change views.py

--- diary/urls.py
+++ diary/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from diary.apps import DiaryConfig
-from diary.views import EntryListView, EntryDetailView, EntryCreateView
+from diary.views import EntryListView, EntryDetailView, EntryCreateView, EntryUpdateView, EntryDeleteView
 
 app_name = DiaryConfig.name
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", EntryListView.as_view(), name="entry_list"),
     path("entrys/<int:pk>/", EntryDetailView.as_view(), name="entry_detail"),
     path("entrys/create", EntryCreateView.as_view(), name="entry_create"),
+    path("entrys/<int:pk>/update/", EntryUpdateView.as_view(), name="entry_update"),
+    path("entrys/<int:pk>/delete/", EntryDeleteView.as_view(), name="entry_delete"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,5 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from diary.models import Entry
 
@@ -11,8 +11,28 @@ class EntryListView(ListView):
 class EntryDetailView(DetailView):
     model = Entry
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
 
 class EntryCreateView(CreateView):
     model = Entry
-    fields = ("title", "content", "image", "publication_sign", "number_of_views")
+    fields = ("title", "content", "image", "publication_sign")
+    success_url = reverse_lazy("diary:entry_list")
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ("title", "content", "image", "publication_sign")
+    success_url = reverse_lazy("diary:entry_list")
+
+    def get_success_url(self):
+        return reverse('diary:entry_detail', args=[self.kwargs.get('pk')])
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
     success_url = reverse_lazy("diary:entry_list")
